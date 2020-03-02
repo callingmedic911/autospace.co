c9b4fb6c0fe933775dad3152c2641f9ce04108ca
Aditya Pandey <aditya@tourio.co>
2020-03-02 16:18:41 +0530

    Update to https

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>http://autospace.co</homePageLink>
+		<homePageLink>https://autospace.co</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://autospace.co/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://autospace.co/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://autospace.co/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://autospace.co/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://autospace.co/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://autospace.co/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://autospace.co/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://autospace.co/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://autospace.co/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://autospace.co/wp-json/" />
 			</apis>
 	</service>
 </rsd>
